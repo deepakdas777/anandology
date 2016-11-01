4953054a88ea42891f66f5efd926a1dca9f861ef
deepakdas777 <deepakdasvkd007@gmail.com>
2016-11-01 16:28:39 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,9 @@
+"""Write a program add.py that takes 2 numbers as command line arguments and prints its sum."""
+
+import sys
+a=sys.argv[1]
+b=sys.argv[2]
+summ=int(a)+int(b)
+print 'SUM=',summ
+
+
